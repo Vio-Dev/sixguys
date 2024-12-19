3c193f4317905f90424464405a6b0970ae444459
Nguyen Duong Hoang Phuc <bmtck0000@gmail.com>
2024-12-19 22:37:53 +0700

    layouts

---
+++ FigureShop/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('title', 'Trang quản lý bán hàng')
+
+@section('content')
+    <div class="w-full">
+        <h1 class="text-2xl font-semibold">Trang quản lý bán hàng</h1>
+
+    </div>
+@endsection
