f26974fc144cd3eae9e59b1e3c84e5f7387e9be9
ducpro <126243561+Vio-Dev@users.noreply.github.com>
2025-01-07 14:05:52 +0700

    update

--- FigureShop/resources/views/admin/blogs/update.blade.php
+++ FigureShop/resources/views/admin/blogs/update.blade.php
@@ -57,7 +57,7 @@
             <div class="mb-5">
                 <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mô tả</label>
                 <textarea name="description" id="description"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                    class="h-[800px] shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
                     placeholder="Nhập nội dung bài đăng">{{ old('description', $post->description) }}</textarea>
                 @error('description')
                     <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
